253bce1e9640ac581f7a2c6be351bb5cda47ef65
graehl <graehl@gmail.com>
2019-08-12 16:43:06 -0700

    text TPU script

--- text/scripts/run_base_uda_tpu.sh
+++ text/scripts/run_base_uda_tpu.sh
@@ -16,18 +16,19 @@ train_tpu=${TPU_ADDRESS:-node-1}
 eval_tpu=${TPU_ADDRESS:-node-2}
 max_seq_length=${1:-${MAX_SEQ_LENGTH:-128}}
 #512
-gsbucket=${2:-xlnet-logs}
-model_dir=gs://$gsbucket/uda/text/ckpt/large_ft_uda_exp_1
+model_dir=${2:-gs://xlnet-logs/uda/text/ckpt/uda_${max_seq_length}}
+init_dir=${3:-pretrained_bert_base}
 
 echo $train_tpu $max_seq_length $model_dir
 
-specargs="--sup_train_data_dir=data/proc_data/IMDB/train_20   --unsup_data_dir=data/proc_data/IMDB/unsup   --eval_data_dir=data/proc_data/IMDB/dev   --bert_config_file=pretrained_models/bert_base/bert_config.json   --vocab_file=pretrained_models/bert_base/vocab.txt   --init_checkpoint=pretrained_models/bert_base/bert_model.ckpt   --task_name=IMDB"
+specargs="--sup_train_data_dir=data/proc_data/IMDB/train_20   --unsup_data_dir=data/proc_data/IMDB/unsup   --eval_data_dir=data/proc_data/IMDB/dev   --bert_config_file=pretrained_models/bert_base/bert_config.json   --vocab_file=pretrained_models/bert_base/vocab.txt    --task_name=IMDB"
 
 python main.py \
   --use_tpu=True \
   --tpu_name=${train_tpu} \
   --do_train=True \
   --do_eval=False \
+  --init_checkpoint=$init_dir
   $specargs \
   --model_dir=${model_dir} \
   --max_seq_length=${max_seq_length} \
